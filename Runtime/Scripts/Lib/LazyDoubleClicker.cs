f31a3a6e2d1907ef94bfd93a4a64c4002be0df63
r-benjamin-cotton <r.benjamin.cotton@outlook.jp>
2025-10-31 11:58:20 +0900

    ...

---
+++ Runtime/Scripts/Lib/LazyDoubleClicker.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace LazyUI
+{
+    /// <summary>
+    /// ダブルクリックを集中管理
+    /// DoubleClickDelayで前のクリックからの許容時間
+    /// DoubleClickDistanceで前のクリックからの許容距離
+    /// </summary>
+    public static class LazyDoubleClicker
+    {
+        public static float DoubleClickDelay = 0.5f;
+        public static float DoubleClickDistance = 4.0f;
+
+        private static double time = 0;
+        private static PointerEventData.InputButton button = 0;
+        private static Vector2 position = Vector2.zero;
+        public static bool OnPointerDown(PointerEventData eventData)
+        {
+            var doubleClick = false;
+            var timeStamp = Time.unscaledTimeAsDouble;
+            var dist = (position - eventData.position).magnitude;
+            var delay = timeStamp - time;
+            if ((button == eventData.button) && (dist < DoubleClickDistance) && (delay < DoubleClickDelay))
+            {
+                doubleClick = true;
+            }
+            position = eventData.position;
+            button = eventData.button;
+            time = timeStamp;
+            return doubleClick;
+        }
+    }
+}
